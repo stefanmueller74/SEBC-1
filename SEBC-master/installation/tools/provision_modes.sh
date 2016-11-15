0fac5c3f9783837668ee87dfdf2bf3bc57263990
Andi <andreas.krisor@advisori.de>
2016-11-15 10:46:27 +0100

    Create provision_modes.sh

---
+++ SEBC-master/installation/tools/provision_modes.sh
@@ -0,0 +1,15 @@
+# Connecting via SSH
+## Edge node
+ssh -i "mykey.pem" ec2-user@54.93.126.39
+## Node1
+ssh -i "mykey.pem" ec2-user@54.93.129.228
+## Node2
+ssh -i "mykey.pem" ec2-user@54.93.221.116
+## Node3
+ssh -i "mykey.pem" ec2-user@54.93.96.233
+
+
+
+
+
+
