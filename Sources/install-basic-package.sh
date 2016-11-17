ce90ba0b8b084fb3789f70d97dcb63fe655a3990
Andi <andreas.krisor@advisori.de>
2016-11-18 00:41:20 +0100

    Update install-basic-package.sh

--- Sources/install-basic-package.sh
+++ Sources/install-basic-package.sh
@@ -24,28 +24,3 @@ echo "Installing: mlocate"
 sudo sudo yum -y install yum-utils
 echo "yum-utils done "
 echo "##################################"
-
-
-################################################################
-# Services
-################################################################
-
-
-echo "##################################"
-echo "Installing: mlocate"
-sudo yum -y install bind-utils
-sudo yum -y install nscd
-service nscd start
-systemctl enable nscd.service
-echo "SERVICE NSCD done "
-echo "##################################"
-
-
-echo "##################################"
-echo "Installing: mlocate"
-sudo yum -y install ntp
-sudo yum -y install nscd
-service nscd start
-systemctl enable ntpd.service
-echo "SERVICE NTP done "
-echo "##################################"
